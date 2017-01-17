6703d3a4e9cc2ccb321930cc6c3b52a801828bdc
Robin Hagman <robin.hagman@op5.com>
2017-01-17 16:59:10 +0100

    Update template to be compatible with rrdtool 1.6
    The template for check_by_snmp_disk and *_proc used the deprecated
    --upper and --lower flags. These should now be --upper-limit and
    --lower-limit to be compatible with rrdtool 1.6.
    This is part of fixing MON-9986.
    
    Signed-off-by: Robin Hagman <robin.hagman@op5.com>

--- op5build/pnp/check_by_snmp_procs.php
+++ op5build/pnp/check_by_snmp_procs.php
@@ -46,7 +46,7 @@ foreach ($this->DS as $KEY=>$VAL) {
 		$warn_min = $VAL['WARN_MIN'];
 	}
 	if ( $VAL['CRIT'] != "" && is_numeric($VAL['CRIT']) ) {
-		$upper = " --upper=" . ($VAL['CRIT'] + 1);
+		$upper = " --upper-limit=" . ($VAL['CRIT'] + 1);
 		$critical = $VAL['CRIT'];
 	}
 	if ( $VAL['CRIT_MAX'] != "" && is_numeric($VAL['CRIT_MAX']) ) {
@@ -56,7 +56,7 @@ foreach ($this->DS as $KEY=>$VAL) {
 		$crit_min = $VAL['CRIT_MIN'];
 	}
 	if ( $VAL['MIN'] != "" && is_numeric($VAL['MIN']) ) {
-		$lower = " --lower=" . $VAL['MIN'];
+		$lower = " --lower-limit=" . $VAL['MIN'];
 		$minimum = $VAL['MIN'];
 	}
 	if ( $VAL['MAX'] != "" && is_numeric($VAL['MAX']) ) {
@@ -64,8 +64,8 @@ foreach ($this->DS as $KEY=>$VAL) {
 	}
 	if ($VAL['UNIT'] == "%%") {
 		$vlabel = "%";
-		$upper = " --upper=101 ";
-		$lower = " --lower=0 ";
+		$upper = " --upper-limit=101 ";
+		$lower = " --lower-limit=0 ";
 	}
 	else {
 		$vlabel = $VAL['UNIT'];
